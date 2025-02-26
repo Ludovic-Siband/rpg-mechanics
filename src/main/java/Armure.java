678cf88f8dde0b86e123b7bd61f22dbbe4467c38
Paul Sabourault <paul.sabourault0@gmail.com>
2025-02-26 09:03:13 +0100

    Résolution de conflits avec le main de Ludo

---
+++ src/main/java/Armure.java
@@ -0,0 +1,18 @@
+import lombok.Data;
+
+@Data
+public class Armure extends Equipement {
+    private String type;
+    private int defense;
+
+    public Armure (String name, int weight, double price,String type, int defense) {
+        super(name, weight, price);
+        this.type = type;
+        this.defense = defense;
+    }
+
+    @Override
+    public String toString() {
+        return "mon armure s'appelle " + this.name + " elle pese " + this.weight + " kg "+" elle coute " + this.price + " piece d'or "+ " c'est un " + this.type + " elle a " + this.defense +" de defenses" ;
+    }
+}
---
+++ src/main/java/Equipement.java
@@ -0,0 +1,19 @@
+import lombok.Data;
+
+@Data
+public class Equipement {
+    public String name;
+    public int  weight;
+    public double price;
+
+    public Equipement(String name, int weight, double price) {
+        this.name = name;
+        this.weight = weight;
+        this.price = price;
+    }
+
+
+
+
+}
+
